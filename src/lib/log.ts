98a0a0d011bae5b800a6c9f9425f413de6ff7f36
UnKnownn <pandaepic01@gmail.com>
2025-02-14 16:43:55 +0530

    clean

--- src/lib/log.ts
+++
@@ -1,8 +0,0 @@
-const log = (message: string, component: string, method: string) => {
-  console.info(
-    `%c[🐺 overwolf-modern-react-boilerplate][🧰 ${component}][🔧 ${method}][📃 ${message} ]`,
-    "background: #262626; color: #e4e4e7; padding: 2px 0; border-radius: 2px; font-weight: bold; border: 1px solid #171717;",
-  );
-};
-
-export { log };
